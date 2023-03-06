cf3b76af23dfd1b0a9f68e1ec78c5ae50ee467a0
Vinit-Premlani <81905387+Vinit-Premlani@users.noreply.github.com>
2023-03-06 23:11:46 +0530

    Add files via upload

---
+++ band genration.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Genrator.")
+city = input("What's name of the city you grow up in ?\n")
+pet = input("What's the name of a pet?\n")
+print("Your band name could be " + city + " "+ pet)
